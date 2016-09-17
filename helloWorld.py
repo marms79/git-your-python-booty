2f55622dc2b9b95228124d1ef0e6daa3fc9485ec
prag717 <brenda.praggastis@pnnl.gov>
2016-09-16 17:29:36 -0700

    added edits for morning workshop

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,15 @@
-a = "Monty Python Rocks!!!"
-print a
-print a + a
-print a*3
-b = a + " " + a
-print b
+### A Math Teacher's idea of Hello World
+print
+print "Hello World! Let's do Math!"
+print
+a = 5
+b = 3
+print "a = ", a
+print "b = ", b
+print "sum: ", a + b  ## Practice some simple math
+print "difference: ", a - b
+print "product: ", a * b
+print "power: ", a**b
+print
+print "{0} divided by {1} equals {2} with a remainder of {3}.".format(a,b,a/b,a%b)
+print
